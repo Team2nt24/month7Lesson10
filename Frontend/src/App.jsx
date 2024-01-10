69207225bbf83e42fa1909f5a91768610b8896a3
BuilderSIA <lordallplay@gmail.com>
2024-01-10 11:21:37 +0500

    username dynamic

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,66 +1,60 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import LoginPage from "./components/LoginPage";
+import { useGlobalContext } from "./context";
+import SidebarLeft from "./components/SidebarLeft";
+import SidebarRight from "./components/SidebarRight";
+import SearchModal from "./components/SearchModal";
+import Footer from "./components/Footer";
+import ComandaPlatte from "./components/ComandaPlatte";
+import CreateNew from "./components/CreateNew";
 
-import { useEffect, useState } from 'react'
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import LoginPage from './components/LoginPage'
-import { useGlobalContext } from './context'
-import SidebarLeft from './components/SidebarLeft';
-import SidebarRight from './components/SidebarRight';
-import SearchModal from './components/SearchModal';
-import Footer from './components/Footer';
-import ComandaPlatte from './components/ComandaPlatte';
-import CreateNew from './components/CreateNew';
-
-
-import Events from './pages/events/Events'
-
-
-
-const url = 'https://api.github.com/users/Muh1isa/received_events'
+import Events from "./pages/events/Events";
 
 export default function App() {
-      const {
-        username,
-        isOpen,
-        isOpenRight, 
-        isOpenSearchModal,
-        comandaPalette,
-        createNew, 
-      } = useGlobalContext();
-      const [eventsList, setEventsList] = useState([])
+  const {
+    username,
+    isOpen,
+    isOpenRight,
+    isOpenSearchModal,
+    comandaPalette,
+    createNew,
+  } = useGlobalContext();
+  const [eventsList, setEventsList] = useState([]);
+
+  const url = `https://api.github.com/users/${username}/received_events`;
 
   const fetchEvents = async () => {
     try {
-      const resp = await fetch(url)
-      const data = await resp.json()
-      setEventsList(data)
+      const resp = await fetch(url);
+      const data = await resp.json();
+      setEventsList(data);
       console.log(data);
     } catch (error) {
-      console.error(error);     
+      console.error(error);
     }
-  }
+  };
 
   useEffect(() => {
-    fetchEvents()
-    
-  }, [])
+    fetchEvents();
+  }, []);
   return (
     <>
-    {!username ? null: <Navbar/>}
-    {isOpen && username ?<SidebarLeft/>:null}
-    {isOpenRight && username ?<SidebarRight/>:null}
-    {isOpenSearchModal && username ? <SearchModal/>:null}
-    {comandaPalette && username ? <ComandaPlatte/> : null}
-    {createNew && username ? <CreateNew/> : null}
+      {!username ? null : <Navbar />}
+      {isOpen && username ? <SidebarLeft /> : null}
+      {isOpenRight && username ? <SidebarRight /> : null}
+      {isOpenSearchModal && username ? <SearchModal /> : null}
+      {comandaPalette && username ? <ComandaPlatte /> : null}
+      {createNew && username ? <CreateNew /> : null}
 
-
-    <Routes>
-      <Route path='/' element={username ? null:<LoginPage/>}/>
-      {/* <Route path='/' element={username ? null:<Footer/>}/> */}
-    </Routes>
-    <Events eventsList={eventsList} />
-    <Footer/>
-    
+      <Routes>
+        <Route path="/" element={username ? null : <LoginPage />} />
+        {/* <Route path='/' element={username ? null:<Footer/>}/> */}
+      </Routes>
+      <Events eventsList={eventsList} />
+      <Footer />
     </>
-  )}
+  );
+}
